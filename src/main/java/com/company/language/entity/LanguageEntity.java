61966cfb235d3abaab0732e00ff60e01ca6932af
j_bakhramov <bahromovjamshid503@gmail.com>
2023-07-20 10:47:22 +0500

    added ViewStatus

--- src/main/java/com/company/language/entity/LanguageEntity.java
+++ src/main/java/com/company/language/entity/LanguageEntity.java
@@ -1,4 +1,18 @@
 package com.company.language.entity;
 
-public class LanguageEntity {
+import com.company.base.EntityBase;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "language_entity")
+public class LanguageEntity extends EntityBase {
+    private String nameUz;
+    private String nameRu;
+    private String nameEng;
+
 }
