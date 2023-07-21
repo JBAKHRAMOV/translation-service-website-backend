045e51285104f70b3880c68592392ebd6a34384b
j_bakhramov <bahromovjamshid503@gmail.com>
2023-07-21 13:12:50 +0500

    added PartnerUpdDto and write all fields

---
+++ src/main/java/com/company/partner/dto/PartnerUpdDto.java
@@ -0,0 +1,12 @@
+package com.company.partner.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PartnerUpdDto {
+    private String icon;
+    private String name;
+    private String link;
+}
